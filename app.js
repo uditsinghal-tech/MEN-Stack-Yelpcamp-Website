b7b76d8867dc41e3452a0fefaa715da7a51fd3e1
Udit <uditsinghal2404@gmail.com>
2020-08-05 14:03:31 +0530

    ss

--- app.js
+++ app.js
@@ -10,16 +10,20 @@ var LocalStrategy=require("passport-local");
 var Campground=require("./models/campground");
 var Comment=require("./models/comment");
 var User=require("./models/user");
+// mongoose.connect("mongodb://localhost:27017/yelp_camp",({useNewUrlParser:true,useUnifiedTopology: true}));
+mongoose.connect(process.env.DATABASEURL,{useNewUrlParser:true,useUnifiedTopology: true});
+// console.log(process.env.DATABASEURL);
 
-mongoose.connect("mongodb+srv://UDIT:udit123@cluster0.bkwum.mongodb.net/<yelp_camp>?retryWrites=true&w=majority",{
-    useNewUrlParser:true,
-    useUnifiedTopology: true,
-    useCreateIndex:true
-}).then(() => {
-    console.log("Db started");
-}).catch(err => {
-    console.log("err"+err.message);
-});
+
+// mongoose.connect("mongodb+srv://UDIT:udit123@cluster0.bkwum.mongodb.net/<yelp_camp>?retryWrites=true&w=majority",{
+//     useNewUrlParser:true,
+//     useUnifiedTopology: true,
+//     useCreateIndex:true
+// }).then(() => {
+//     console.log("Db started");
+// }).catch(err => {
+//     console.log("err"+err.message);
+// });
 
 var commentRoutes = require("./routes/comments");
 var campgroundRoutes= require("./routes/campgrounds");
