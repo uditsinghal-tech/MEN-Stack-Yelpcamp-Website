d95c58bbc9316ad3be9b71078e832e63a8e3bab4
Udit <uditsinghal2404@gmail.com>
2020-08-05 10:01:44 +0530

    add app

--- app.js
+++ app.js
@@ -57,6 +57,6 @@ app.use(indexRoutes);
 app.use("/campgrounds",campgroundRoutes);
 app.use("/campgrounds/:id/comments",commentRoutes);
 
-app.listen(3000,process.env.IP,function(){
+app.listen(process.env.PORT,process.env.IP,function(){
     console.log("Server has started");
 });
