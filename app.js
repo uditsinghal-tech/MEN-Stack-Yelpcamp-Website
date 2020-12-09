839c6a7ffb8cc94a502a04ce2892b203cdc17f75
Udit <uditsinghal2404@gmail.com>
2020-12-09 14:22:04 +0530

    commit

--- app.js
+++ app.js
@@ -10,7 +10,16 @@ var LocalStrategy=require("passport-local");
 var Campground=require("./models/campground");
 var Comment=require("./models/comment");
 var User=require("./models/user");
-mongoose.connect("mongodb://localhost:27017/yelp_camp",({useNewUrlParser:true,useUnifiedTopology: true}));
+//mongoose.connect("mongodb://localhost:27017/yelp_camp",({useNewUrlParser:true,useUnifiedTopology: true}));
+mongoose.connect("mongodb+srv://UDIT:udit2404@cluster0.3bmu1.mongodb.net/<yelp_camp>?retryWrites=true&w=majority",{
+    useNewUrlParser:true,
+    useUnifiedTopology: true,
+    useCreateIndex:true
+}).then(() => {
+    console.log("Db started");
+}).catch(err => {
+    console.log("err"+err.message);
+});
 //mongoose.connect(process.env.DATABASEURL,{useNewUrlParser:true,useUnifiedTopology: true});
 // console.log(process.env.DATABASEURL);
 
